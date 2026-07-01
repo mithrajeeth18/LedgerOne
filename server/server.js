06fc82154d4d2e16efec0e906570311f7c598609
Mithrajeeth18 <mithra86753@gmail.com>
2026-07-01 12:05:24 +0530

    dummy

--- server/server.js
+++ server/server.js
@@ -5,6 +5,12 @@ const { connectDB } = require('./src/config/db');
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'LedgerOne API Running 🚀',
+  });
+});
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
